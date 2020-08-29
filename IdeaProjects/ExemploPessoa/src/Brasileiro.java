f3e7ed43e029f00afcc35c60544d87af153a2520
Mila9 <barbosa196milena@gmail.com>
2020-08-29 19:09:12 -0300

    Primeiro Commit

--- IdeaProjects/ExemploPessoa/src/Brasileiro.java
+++
@@ -1,7 +0,0 @@
-import java.util.Date;
-
-public class Brasileiro extends Pessoa {
-    public Brasileiro(String _nome, String _cpf, Date _dataNascimento, String _estadoCivil){
-        super(_nome, _cpf, _dataNascimento, _estadoCivil);
-    }
-}
--- IdeaProjects/ExemploPessoa/src/Dinamarques.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class Dinamarques extends Pessoa {
-    public Dinamarques(String _nome, String _cpf, Date _dataNascimento, String _estadoCivil) {
-        super(_nome, _cpf, _dataNascimento, _estadoCivil);
-    }
-
-
-}
